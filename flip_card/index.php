2bc6c4007e0b8da6178a6dd5d6e45023e9b2763e
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-09-06 19:13:43 +0100

    flip-carde

---
+++ flip_card/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Flip Card</title>
+</head>
+
+<body>
+    <div class="flip-card flex">
+        <div class="flip-card-inner flex">
+            <div class="flip-card-front flex">
+                <div class="flip-card-front-image flex">
+                    <div class="circle_service">
+                        <img src="image/icons_btp.svg" alt="Avatar">
+                    </div>
+                    <p>Bâtiments & Travaux Publics</p>
+                </div>
+            </div>
+            <div class="flip-card-back">
+                <div class="flip-card-back-image">
+                    <img src="image/service_btp.png" alt="">
+                </div>
+                <div class="flip-card-back-filter">
+                    <div class="flip-card-back-circle flex">
+                        <div class="circle_service_back">
+                            <img src="image/icons_btp.svg" alt="Avatar">
+                        </div>
+                        <div class="circle_service_text">
+                            <p>Bâtiments & Travaux Publics</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- input_img/index.php
+++ input_img/index.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.scss">
     <title>Document</title>
 </head>
+
 <body>
-<div class="">
-            <div class="icon-wrapper" id="uploadIcon1">
-                <div class="imageviw">
-                    <img class="imagep" id="imagePreview1" src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" style="max-width: 300px;">
-                </div>
+    <div class="">
+        <div class="icon-wrapper" id="uploadIcon1">
+            <div class="imageviw">
+                <img class="imagep" id="imagePreview1"
+                    src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+                    style="max-width: 300px;">
             </div>
-            <input type="file" name="profile_image" id="imageInput1" accept="image/*"> 
-        </div> 
-<script src="script.js"></script>
+        </div>
+        <input type="file" name="profile_image" id="imageInput1" accept="image/*">
+    </div>
+    <script src="script.js"></script>
 </body>
+
 </html>
